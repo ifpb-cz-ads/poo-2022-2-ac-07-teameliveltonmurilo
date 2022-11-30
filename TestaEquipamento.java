4a65bd528ccbdc8160f9dfeb8571a3e5454fe4b2
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2022-11-30 14:49:53 -0300

    Livro Batista e Moraes (2013) Questão 8

--- TestaEquipamento.java
+++ TestaEquipamento.java
@@ -10,5 +10,14 @@ public class TestaEquipamento {
         comp.setAtributo3("Terceiro Atributo");
         comp.setAtributo4("Quarto Atributo");
 
+        System.out.println("Primeiro objeto");
+        System.out.println("Atributo 1: " + equip.getAtributo1());
+        System.out.println("Atributo 2: " + equip.getAtributo2());
+        System.out.println("Segundo objeto");
+        System.out.println("Atributo 1: " + comp.getAtributo1());
+        System.out.println("Atributo 2: " + comp.getAtributo2());
+        System.out.println("Atributo 3: " + comp.getAtributo3());
+        System.out.println("Atributo 4: " + comp.getAtributo4());
+        
     }
 }
