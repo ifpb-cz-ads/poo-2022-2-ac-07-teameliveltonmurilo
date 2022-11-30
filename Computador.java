2df42f5c4163d67a8b1ffa0123c23c65c9d9de6a
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2022-11-30 14:40:33 -0300

    Livro Batista e Moraes (2013) Questão 6

--- Computador.java
+++ Computador.java
@@ -1,4 +1,20 @@
 public class Computador extends Equipamento{
     String atributo3;
     String atributo4;
+
+    public String getAtributo3(){
+        return this.atributo3;
+    }
+
+    public void setAtributo3(String str){
+        this.atributo3 = str;
+    }
+
+    public String getAtributo4(){
+        return this.atributo4;
+    }
+    
+    public void setAtributo4(String str){
+        this.atributo4 = str;
+    }
 }
--- Equipamento.java
+++ Equipamento.java
@@ -1,4 +1,20 @@
 public class Equipamento{
     private String atributo1;
     private String atributo2;
+
+    public String getAtributo1(){
+        return this.atributo1;
+    }
+    
+    public void setAtributo1(String str){
+        this.atributo1 = str;
+    }
+    
+    public String getAtributo2(){
+        return this.atributo2;
+    }
+    
+    public void setAtributo2(String str){
+        this.atributo2 = str;
+    }
 }
