526c2adef5517f20479d4a8b644eb9a84171b571
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2022-11-30 14:47:49 -0300

    Livro Batista e Moraes (2013) Questão 7

---
+++ TestaEquipamento.java
@@ -0,0 +1,14 @@
+public class TestaEquipamento {
+    public static void main(String[] args){
+        Equipamento equip = new Equipamento();
+        equip.setAtributo1("Primeiro Atributo");
+        equip.setAtributo2("Segundo Atributo");
+
+        Computador comp = new Computador();
+        comp.setAtributo1("Primeiro Atributo");
+        comp.setAtributo2("Segundo Atributo");
+        comp.setAtributo3("Terceiro Atributo");
+        comp.setAtributo4("Quarto Atributo");
+
+    }
+}
